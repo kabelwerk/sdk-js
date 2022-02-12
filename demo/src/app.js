3ebb6a461f8c50a25dc7ffc871add28e5d63ba4d
pavelsof <mail@pavelsof.com>
2022-02-12 18:38:33 +0100

    React demo: hello world.

---
+++ demo/src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = function () {
+    return <h1>Hello world!</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
