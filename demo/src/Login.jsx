657a8f14d8319d8ee3c99e8375f2864e59e83bf9
pavelsof <mail@pavelsof.com>
2022-04-14 16:13:30 +0200

    React demo: {onBlur,onFocus → onChange} login form validation.

--- demo/src/Login.jsx
+++ demo/src/Login.jsx
@@ -8,46 +8,64 @@ import {
 import React from 'react';
 import { formatText } from './utils/string';
 
-const validateLogin = (config) => {
-    const { url, token } = config;
+// return an error message if the given value is not a valid URL
+const validateUrl = function (url) {
+    if (url.length === 0) {
+        return 'URL cannot be empty.';
+    }
 
-    const validateUrl = (url) => {
-        if (url.length === 0) {
-            return 'URL cannot be empty.';
-        }
+    if (!/^(ws|wss):\/\/[^ "]+$/.test(url)) {
+        return 'Not a valid WebSocket URL.';
+    }
 
-        if (/^(ws|wss):\/\/[^ "]+$/.test(url) === false) {
-            return 'Not a valid URL.';
-        }
+    return '';
+};
 
-        return '';
-    };
+// return an error message if the given value does not look like a valid token
+const validateToken = function (token) {
+    if (token.length === 0) {
+        return 'Token cannot be empty.';
+    }
 
-    const validateToken = (token) => {
-        if (token.length === 0) {
-            return 'Token cannot be empty.';
-        }
-
-        return '';
-    };
-
-    return {
-        urlValidationMessage: validateUrl(url),
-        tokenValidationMessage: validateToken(token),
-        areAllFormFieldsValid:
-            validateUrl(url).length === 0 && validateToken(token).length === 0,
-    };
+    return '';
 };
 
 const Login = function ({ config, updateConfig }) {
+    // the form field values
     const [url, setUrl] = React.useState(config.url);
     const [token, setToken] = React.useState(config.token);
     const [name, setName] = React.useState(config.name);
-    const [formErrors, setFormErrors] = React.useState({
-        urlValidationMessage: '',
-        tokenValidationMessage: '',
-        areAllFormFieldsValid: false,
-    });
+
+    // the form field errors
+    const [urlError, setUrlError] = React.useState('');
+    const [tokenError, setTokenError] = React.useState('');
+
+    // handle change events for the URL input
+    const handleUrlChange = function (e) {
+        const value = formatText(e.target.value);
+
+        setUrl(value);
+        setUrlError(validateUrl(value));
+    };
+
+    // handle change events for the token input
+    const handleTokenChange = function (e) {
+        const value = formatText(e.target.value);
+
+        setToken(value);
+        setTokenError(validateToken(value));
+    };
+
+    // handle change events for the name input
+    const handleNameChange = function (e) {
+        setName(formatText(e.target.value));
+    };
+
+    // handle form submit events
+    const handleSubmit = function (e) {
+        e.preventDefault();
+        updateConfig({ url, token, name });
+    };
 
     return (
         <Pane
@@ -56,86 +74,64 @@ const Login = function ({ config, updateConfig }) {
             alignItems="center"
             height="100vh"
         >
-            <Pane padding={32} elevation={4} borderRadius={20} width={400}>
-                <Pane>
-                    <Heading size={800} marginBottom={32}>
-                        Connect
-                    </Heading>
-                </Pane>
-                <Pane marginTop={16}>
-                    <TextInputField
-                        label="URL"
-                        placeholder="URL"
-                        isInvalid={formErrors.urlValidationMessage.length > 0}
-                        onChange={(e) => setUrl(formatText(e.target.value))}
-                        validationMessage={
-                            formErrors.urlValidationMessage.length === 0
-                                ? undefined
-                                : formErrors.urlValidationMessage
-                        }
-                        onBlur={() =>
-                            setFormErrors(validateLogin({ url, token }))
-                        }
-                        onFocus={() =>
-                            setFormErrors({
-                                urlValidationMessage: '',
-                                tokenValidationMessage:
-                                    formErrors.tokenValidationMessage,
-                            })
-                        }
-                        value={url}
-                        type="url"
-                        required
-                    />
-                </Pane>
-                <Pane marginTop={16}>
-                    <TextInputField
-                        label="Token"
-                        placeholder="Token"
-                        isInvalid={formErrors.tokenValidationMessage.length > 0}
-                        onChange={(e) => setToken(formatText(e.target.value))}
-                        validationMessage={
-                            formErrors.tokenValidationMessage.length === 0
-                                ? undefined
-                                : formErrors.tokenValidationMessage
-                        }
-                        onBlur={() =>
-                            setFormErrors(validateLogin({ url, token }))
-                        }
-                        onFocus={() =>
-                            setFormErrors({
-                                urlValidationMessage:
-                                    formErrors.urlValidationMessage,
-                                tokenValidationMessage: '',
-                            })
-                        }
-                        required
-                    />
-                </Pane>
-                <Pane marginTop={16}>
-                    <TextInputField
-                        label="Name"
-                        description="The displayed name in the chat"
-                        placeholder="Name"
-                        onChange={(e) => setName(formatText(e.target.value))}
-                        value={name}
-                    />
-                </Pane>
-                <Pane display="flex" justifyContent="flex-end" marginTop={32}>
-                    <Button
-                        appearance="primary"
-                        size="large"
-                        iconBefore={DataConnectionIcon}
-                        onClick={(e) => {
-                            e.preventDefault();
-                            updateConfig({ url, token, name });
-                        }}
-                        disabled={!formErrors.areAllFormFieldsValid}
+            <form onSubmit={handleSubmit}>
+                <Pane padding={32} elevation={4} borderRadius={20} width={400}>
+                    <Pane>
+                        <Heading size={800} marginBottom={32}>
+                            Connect
+                        </Heading>
+                    </Pane>
+                    <Pane marginTop={16}>
+                        <TextInputField
+                            value={url}
+                            required
+                            label="URL"
+                            placeholder="URL"
+                            onChange={handleUrlChange}
+                            isInvalid={!!urlError}
+                            validationMessage={urlError ? urlError : undefined}
+                        />
+                    </Pane>
+                    <Pane marginTop={16}>
+                        <TextInputField
+                            required
+                            label="Token"
+                            placeholder="Token"
+                            onChange={handleTokenChange}
+                            isInvalid={!!tokenError}
+                            validationMessage={
+                                tokenError ? tokenError : undefined
+                            }
+                        />
+                    </Pane>
+                    <Pane marginTop={16}>
+                        <TextInputField
+                            value={name}
+                            label="Name"
+                            description="The displayed name in the chat"
+                            placeholder="Name"
+                            onChange={handleNameChange}
+                        />
+                    </Pane>
+                    <Pane
+                        display="flex"
+                        justifyContent="flex-end"
+                        marginTop={32}
                     >
-                        Connect
-                    </Button>
+                        <Button
+                            type="submit"
+                            appearance="primary"
+                            size="large"
+                            iconBefore={DataConnectionIcon}
+                            disabled={
+                                !url || !!urlError || !!tokenError || !token
+                            }
+                        >
+                            Connect
+                        </Button>
+                    </Pane>
                 </Pane>
-            </Pane>
+            </form>
         </Pane>
     );
 };
